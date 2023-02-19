e1d794db2adf0b98f1fa6b417e33c6a360dd3a7f
strawstack <rhshadowgreen@gmail.com>
2023-02-18 18:10:27 -0800

    Draggable nodes

--- script.js
+++ script.js
@@ -26,6 +26,38 @@
         }
     }
 
+    function uidFactory() {
+        let id = 0;
+        return () => {
+            const temp = id;
+            id += 1;
+            return temp;
+        };
+    }
+
+    function getMouse(e) {
+        return {
+            x: e.clientX,
+            y: e.clientY
+        };
+    }
+    function add(a, b) {
+        return {
+            x: a.x + b.x,
+            y: a.y + b.y,
+        };
+    }
+    function sub(a, b) {
+        return {
+            x: a.x - b.x,
+            y: a.y - b.y,
+        };
+    }
+
+    function translate(elem, {x, y}) {
+        elem.setAttribute("transform", `translate(${x} ${y})`);
+    }
+
     function createNodeFactory(state, create) {
         return (add, opts) => {
             opts = {...opts, ...{
@@ -40,19 +72,20 @@
             });
 
             // Node background
-            let r = create("rect", false, {
+            let nb = create("rect", false, {
                 class: "node-body",
                 x: -opts.width/2, 
                 y: -opts.height/2,
                 width: opts.width,
                 height: opts.height
             });
-            g.appendChild(r);
+            g.appendChild(nb);
 
-            const in_spacing = state.const.connect_spacing[opts.in - 1];
+            const in_spacing  = state.const.connect_spacing[opts.in - 1];
             const out_spacing = state.const.connect_spacing[opts.out - 1];
 
             // Inputs
+            const c_in = [];
             for (let i = 0; i < opts.in; i++) {
                 let c = create("circle", false, {
                     class: "in-connect",
@@ -60,10 +93,12 @@
                     cy: in_spacing[i],
                     r: state.const.connect_rad
                 });
+                c_in.push(c);
                 g.appendChild(c);
             }
 
             // Outputs
+            const c_out = [];
             for (let i = 0; i < opts.out; i++) {
                 let c = create("circle", false, {
                     class: "out-connect",
@@ -71,11 +106,44 @@
                     cy: out_spacing[i],
                     r: state.const.connect_rad
                 });
+                c_out.push(c);
                 g.appendChild(c);
             }
 
-            
+            return {g, nb, c_in, c_out};
         };
+    }
+
+    function render(state) {
+
+        // For each node 
+        for (let k in state.nodes) {
+            let ns = state.nodes[k];
+
+            // Create if necessary
+            if (ns.ref === null) {
+                const {g, nb, c_in, c_out} = state.helper.createNode(false, ns.opts);
+                ns.ref = g;
+                for (let conn of c_in) {
+                    let cid = state.uid();
+                    ns.in[cid] = {ref: conn};
+                }
+                for (let conn of c_out) {
+                    let cid = state.uid();
+                    ns.out[cid] = {ref: conn};
+                }
+                nb.addEventListener("mousedown", () => {
+                    ns.mousedown(state, k);
+                });
+                nb.addEventListener("mouseup", () => {
+                    ns.mouseup(state);
+                });
+                state.svg.appendChild(g);
+            }
+
+            // Sync node positon
+            translate(ns.ref, {x: ns.opts.x, y: ns.opts.y});
+        }
     }
 
     function main() {
@@ -88,7 +156,22 @@
         svg.setAttribute("viewport", `0 0 ${width} ${height}`);
 
         const connect_rad = 20;
+        const uid = uidFactory();
         const state = {
+            /* 
+            nodes: {
+                key: {
+                    opts: {},
+                    in: {key: {state:, ref: null}},
+                    out: {key: {state:, ref: null}},
+                    ref: null
+                }
+            } */
+            nodes: {},
+            mouse: null,
+            selected_node: null,
+            selected_offset: {x: null, y: null},
+            uid: uid,
             const: {
                 node_width: 250,
                 node_height: 250,
@@ -98,26 +181,58 @@
                     [-3 * connect_rad, 3 * connect_rad],
                     [-3 * connect_rad, 0, 3 * connect_rad]
                 ]
-            }
+            },
+            svg: svg
         };
 
         const create     = createFactory(state, svg);
         const createNode = createNodeFactory(state, create);
+        state.helper = {create, createNode};
 
-        press("c", (e) => {
-            /*
-            createNode(true, {
-                x: 100,
-                y: 100
-            }); */
+        press("c", () => {
+            const m = state.mouse;
+            if (m !== null) {
+                const nid = state.uid();
+                state.nodes[nid] = {
+                    opts: {
+                        x: m.x,
+                        y: m.y,
+                        in: 1,
+                        out: 2
+                    },
+                    in: {},
+                    out: {},
+                    ref: null,
+                    mousedown: (state, nid) => {
+                        const m = state.mouse;
+                        const opts = state.nodes[nid].opts;
+                        state.selected_node = nid;
+                        state.selected_offset = sub({x: opts.x, y: opts.y}, m);
+                    },
+                    mouseup: (state) => {
+                        state.selected_node = null;
+                        state.selected_offset = null;
+                    }
+                };
+                render(state);
+            }
         });
 
-        createNode(true, {
-            x: 284,
-            y: 321,
-            in: 1,
-            out: 2
+        window.addEventListener("mousemove", (e) => {
+            state.mouse = getMouse(e);
+
+            if (state.selected_node !== null) {
+                const opts = state.nodes[state.selected_node].opts;
+                const newPos = add(state.mouse, state.selected_offset);
+                opts.x = newPos.x;
+                opts.y = newPos.y;
+            }
+
+            render(state);
         });
+
+        // init
+        render(state);
     }
 
     main();
